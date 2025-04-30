8ffa99ecd30d4a5ab8e82bbe3ef6f0818199659e
Sapana Gupta <146055257+sapanagupta25@users.noreply.github.com>
2025-04-30 15:23:18 +0530

    Update Products.js

--- src/components/constants/Products.js
+++ src/components/constants/Products.js
@@ -1,17 +1,9 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+const products = [
+  { id: 1, name: "Mobile 1", description: "Description 1", price: "$200" },
+  { id: 2, name: "Mobile 2", description: "Description 2", price: "$300" },
+  { id: 3, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 4, name: "Mobile 4", description: "Description 3", price: "$400" },
+  { id: 5, name: "Mobile 5", description: "Description 3", price: "$400" },
+];
 
-const Product = () => {
-  const { id } = useParams();
-
-  return (
-    <div>
-      <h1>Product {id} </h1>
-      <Link to="/">
-        <button className="btn">Other Products</button>
-      </Link>
-    </div>
-  );
-};
-
-export default Product;
+export default products;
