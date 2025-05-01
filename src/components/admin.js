475da957e4056da55f8dcdcdb393190dcc49d562
Sapana Gupta <146055257+sapanagupta25@users.noreply.github.com>
2025-05-01 17:37:11 +0530

    Update admin.js

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,5 +1,5 @@
 import React from "react";
-import products from "../constants/products";
+import products from "../constants/product";
 import { Link } from "react-router-dom";
 
 function Admin() {
